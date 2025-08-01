3c434beea2e654c9a9f50194d92fb7905949c2db
Kalicz Péter <kaliczp@gmail.com>
2025-08-01 10:00:42 +0200

    Add CRS and 3D plot

--- Readme.R
+++ Readme.R
@@ -3,7 +3,13 @@ library(lidR)
 las <- readLAS("Ground_points.las")
 plot(las)
 
+install.packages('lasR', repos = 'https://r-lidar.r-universe.dev')
+library(lasR)
+
 ## Read ERS
 library(terra)
 ers <- rast("DTM.ers")
 plot(ers)
+## Correct missing coordsystem
+crs(ers) <- "epsg:23700"
+plot_dtm3d(ers)
