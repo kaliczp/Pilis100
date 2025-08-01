65c96ea4da56ecefe73b7587f39f4fee65c245b7
Kalicz Péter <kaliczp@gmail.com>
2025-08-01 15:46:22 +0200

    Improved scripts

--- ERSscript.R
+++ ERSscript.R
@@ -1,5 +1,7 @@
+print("Start")
 library(lidR)
 set_lidr_threads(3)
+cat("Configured lidR threads: ")
 get_lidr_threads()
 las <- readLAS("Ground_points.las")
 
@@ -23,6 +25,7 @@ p100ers <- rast("pilis100dtm.ers")
 p100fill <- rasterize_canopy(las, res = 0.5, algorithm = p2r(subcircle = 0.2, na.fill = tin()))
 terra::crs(p100fill) <- "epsg:23700"
 p100mask <- focal(p100ers,w=3, mean, na.pol = "only", na.rm = TRUE)
-writeRaster(p100mask, "focal.ers", filetype = "ERS")
+writeRaster(p100mask, "focal.ers", filetype = "ERS", overwrite = TRUE)
 p100fillm <- mask(p100fill, p100mask)
-writeRaster(p100fillm, "TIN.ers", filetype = "ERS")
+writeRaster(p100fillm, "TIN.ers", filetype = "ERS", overwrite = TRUE)
+cat("End\n")
--- ersRjob.sh
+++ ersRjob.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name="ERS R teszt job"
-#SBATCH --time=10:00
+#SBATCH --time=3:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=2000
 #SBATCH --output=ersRjob.out
 
+module list
 module swap PrgEnv-cray PrgEnv-gnu
 module load cmake cray-python cray-parallel-netcdf cray-R
+module list
 
 # Set $PATH
 PATH=/project/p_erdlidar/local/bin:$PATH
