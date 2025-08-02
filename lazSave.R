715ef8569ac0b1ebdd6b4f75bb1aaca144b02d88
Kalicz Péter <kaliczp@gmail.com>
2025-08-02 22:39:04 +0200

    Change filename

--- lazSave.R
+++ lazSave.R
@@ -3,7 +3,7 @@ library(lidR)
 set_lidr_threads(3)
 cat("Configured lidR threads: ")
 get_lidr_threads()
-lasname <- "41"
+lasname <- "33"
 las <- readLAS(paste0(lasname, ".las"))
 writeLAS(las, paste0(lasname, ".laz"))
 cat("End script.\n")
