1852e3aa5fabfd5a315607b08559cd9971c0315e
Kalicz Péter <kaliczp@gmail.com>
2025-08-11 20:11:58 +0200

    Test csf()

--- ProcessFull.R
+++ ProcessFull.R
@@ -1,2 +1,8 @@
 library(lidR)
 las <- readLAS("41.laz")
+## Default csf
+las <- classify_ground(las, algorithm = csf())
+## Custom parameter csf
+mycsf <- csf(sloop_smooth = TRUE, class_threshold = 0.4, cloth_resolution = 0.8, time_step = 0.9)
+las <- classify_ground(las, mycsf)
+gnd <- filter_ground(las)
