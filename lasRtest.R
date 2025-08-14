87f39ec19b469ad9fa39903f777377034c2628ad
Kalicz Péter <kaliczp@gmail.com>
2025-08-14 17:13:14 +0200

    Name outputs explicitly

--- lasRtest.R
+++ lasRtest.R
@@ -1,5 +1,5 @@
 library(lasR)
 tempdir()
 folder = "./"
-pipeline = classify_with_sor() + delete_noise() + chm(1, ofile = "chm.tif") + dtm(1, ofile = "dtm.tif")
+pipeline = classify_with_sor() + delete_noise() + chm(0.5, ofile = "chm.tif") + dtm(0.5, ofile = "dtm.tif")
 exec(pipeline, on = folder, ncores = concurrent_points(ncores = 5), progress = TRUE)
