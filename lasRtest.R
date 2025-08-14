1e6ebddc1a9daacbd0262840410e6fe237172f38
Kalicz Péter <kaliczp@gmail.com>
2025-08-14 17:12:46 +0200

    Output tempdir location

--- lasRtest.R
+++ lasRtest.R
@@ -1,4 +1,5 @@
 library(lasR)
+tempdir()
 folder = "./"
 pipeline = classify_with_sor() + delete_noise() + chm(1, ofile = "chm.tif") + dtm(1, ofile = "dtm.tif")
 exec(pipeline, on = folder, ncores = concurrent_points(ncores = 5), progress = TRUE)
