d324d08ab88e0743564c945058a291c521fc5cff
Kalicz Péter <kaliczp@gmail.com>
2025-08-14 17:11:36 +0200

    Explicit output names

--- lasRtest.R
+++ lasRtest.R
@@ -1,4 +1,4 @@
 library(lasR)
 folder = "./"
-pipeline = classify_with_sor() + delete_noise() + chm(1) + dtm(1)
+pipeline = classify_with_sor() + delete_noise() + chm(1, ofile = "chm.tif") + dtm(1, ofile = "dtm.tif")
 exec(pipeline, on = folder, ncores = concurrent_points(ncores = 5), progress = TRUE)
