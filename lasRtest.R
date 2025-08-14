998b65a104da5bafd0109d2bc558947ebe72cbd4
Kalicz Péter <kaliczp@gmail.com>
2025-08-14 18:13:56 +0200

    Try to write classified pointcloud

--- lasRtest.R
+++ lasRtest.R
@@ -1,5 +1,5 @@
 library(lasR)
 tempdir()
 folder = "./"
-pipeline = classify_with_sor() + delete_noise() + chm(0.5, ofile = "chm.tif") + dtm(0.5, ofile = "dtm.tif")
+pipeline = classify_with_sor() + delete_noise() + chm(0.5, ofile = "chm.tif") + dtm(0.5, ofile = "dtm.tif") + write_las(ofile = "*class.laz"))
 exec(pipeline, on = folder, ncores = concurrent_points(ncores = 5), progress = TRUE)
