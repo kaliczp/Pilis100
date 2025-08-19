d9b476bd961cbac04e05964722fe92216e739479
Kalicz Péter <kaliczp@gmail.com>
2025-08-19 09:13:33 +0200

    Overwrite raster
    Overwrite raster in the case of multiple run

--- MakeERS.R
+++ MakeERS.R
@@ -20,5 +20,5 @@ ctg1 <- classify_ground(ctg, algorithm = csf())
 dtm_tin <- rasterize_terrain(ctg1, res = 1, algorithm = tin())
 library(terra)
 terra::crs(dtm_tin) <- "epsg:23700"
-writeRaster(dtm_tin,paste0(ActFile,"dem.ers"), filetype = "ERS")
+writeRaster(dtm_tin,paste0(ActFile,"dem.ers"), filetype = "ERS",overwrite=TRUE)
 cat("End\n")
