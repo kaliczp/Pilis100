f7fe19dc30a97f410d593c20ded80e92c8726f66
Kalicz Péter <kaliczp@gmail.com>
2025-08-20 10:39:00 +0200

    Save whole catalog as laz

---
+++ RunRscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="R script"
+#SBATCH --time=00:20:00
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=2000
+#SBATCH --error=RScript.%J.stderr
+#SBATCH --output=RScript.%J.stdout
+
+# Set $PATH
+PATH=/project/p_erdlidar/local/bin:$PATH
+export PATH
+LD_LIBRARY_PATH=/project/p_erdlidar/local/lib64:/project/p_erdlidar/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+PKG_CONFIG_PATH=/project/p_erdlidar/local/lib64/pkgconfig:/project/p_erdlidar/local/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH
+echo $PATH
+# Run script
+cd /scratch/p_erdlidar/Pilis100
+Rscript ./lazSaveCatalog.R
---
+++ lazSaveCatalog.R
@@ -0,0 +1,16 @@
+cat("Start script\n")
+nothreads <- 10L
+library(lidR)
+library(future)
+plan(multicore, workers = nothreads)
+set_lidr_threads(nothreads)
+cat("Configured lidR threads: ")
+get_lidr_threads()
+ctg <- readLAScatalog("vgyok/")
+st_crs(ctg) <- 23700
+opt_chunk_buffer(ctg) <- 0
+opt_chunk_size(ctg) <- 0
+opt_laz_compression(ctg) <- TRUE
+opt_output_files(ctg) <- "vgylaz/{ORIGINALFILENAME}"
+newctg = catalog_retile(ctg)
+cat("End script.\n")
