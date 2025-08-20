e240867bcdf35830059dfffff5066fae5f98a3d5
Kalicz Péter <kaliczp@gmail.com>
2025-08-20 11:30:04 +0200

    Add a hopefully harmless filter

--- lazSaveCatalog.R
+++ lazSaveCatalog.R
@@ -11,6 +11,7 @@ st_crs(ctg) <- 23700
 opt_chunk_buffer(ctg) <- 0
 opt_chunk_size(ctg) <- 0
 opt_laz_compression(ctg) <- TRUE
+opt_filter(ctg) <- "-drop_z_below 1"
 opt_output_files(ctg) <- "vgylaz/{ORIGINALFILENAME}"
 newctg = catalog_retile(ctg)
 cat("End script.\n")
