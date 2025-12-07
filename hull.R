7750e263e8895c93397d8f6c0589a315c9093919
Péter Kalicz <kaliczp@gmail.com>
2025-12-07 11:05:18 +0100

    Idea to import all files box into one object

--- hull.R
+++ hull.R
@@ -1,8 +1,15 @@
-library(lasR)
 library(lidR)
 Path <- "/home/kaliczp/mnt/disk/Munka/2022DINPIBörzsöny/Adatok/LiDAR_adat/Nyers_pontfelho_UTM34"
-FileName <- "RAW_LiDAR_01.las"
-f <- paste(Path, FileName, sep = "/")
-las <- readLAS(f)
-bboxpoints <- st_bbox(las)
-bboxarea <- st_as_sfc(area)
+
+for(FileName in dir(Path)) {
+    f <- paste(Path, FileName, sep = "/")
+    las <- readLAS(f)
+    bboxpoints <- st_bbox(las)
+    bboxarea <- st_as_sfc(area)
+    bboxarea$File <- substr(FileName, 11, 12)
+    if(FileName == "RAW_LiDAR_01.las") {
+        bboxareaall <- bboxarea
+    } else {
+        bboxareaall <- c(bboxareaall, bboxarea)
+    }
+}
