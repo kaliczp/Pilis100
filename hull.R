6d78cfb6c8880645086bd753e75c1ad0141fe6dc
Péter Kalicz <kaliczp@gmail.com>
2025-12-07 14:56:10 +0100

    Get all files and extrect bbox

--- hull.R
+++ hull.R
@@ -1,15 +1,16 @@
 library(lidR)
+library(sf)
 Path <- "/home/kaliczp/mnt/disk/Munka/2022DINPIBörzsöny/Adatok/LiDAR_adat/Nyers_pontfelho_UTM34"
 
 for(FileName in dir(Path)) {
     f <- paste(Path, FileName, sep = "/")
     las <- readLAS(f)
     bboxpoints <- st_bbox(las)
-    bboxarea <- st_as_sfc(area)
-    bboxarea$File <- substr(FileName, 11, 12)
+    bboxarea <- st_sf(data.frame(FileNr = substr(FileName, 11, 12),
+                                 geom = st_as_sfc(bboxpoints)))
     if(FileName == "RAW_LiDAR_01.las") {
         bboxareaall <- bboxarea
     } else {
-        bboxareaall <- c(bboxareaall, bboxarea)
+        bboxareaall <- rbind(bboxareaall, bboxarea)
     }
 }
