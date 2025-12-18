257eef13077e020665b1ac81f8458d197494d051
Péter Kalicz <kaliczp@gmail.com>
2025-12-18 17:01:42 +0100

    Initial outline extraction
    Outline get from the density raster

---
+++ getOutline.R
@@ -0,0 +1,18 @@
+cat("Start script\n")
+library(lidR)
+set_lidr_threads(3)
+cat("Configured lidR threads: ")
+get_lidr_threads()
+lazNameAll <- dir(patt="laz$")
+for(lasname in lazNameAll){
+    las <- readLAS(paste0(lasname))
+    density <- rasterize_density(las, res = 10)
+    reclass <- c(0, Inf, 1)
+    reclass_m <- matrix(reclass, ncol = 3, byrow = TRUE)
+    densClass <- terra::classify(density, reclass_m)
+    densClass[densClass == 0] <- NA
+    lasVect <- as.polygons(densClass)
+    lassf <- sf::st_as_sf(lasVect)
+    lassfc <- sf::st_as_sfc(lassf)
+}
+cat("End script.\n")
