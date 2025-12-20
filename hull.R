7221a050d5d22f9006c3d78bb59f06384d3aba0f
Péter Kalicz <kaliczp@gmail.com>
2025-12-20 13:56:10 +0100

    Rationalise file read
    Select only laz files

--- hull.R
+++ hull.R
@@ -2,13 +2,15 @@ library(lidR)
 library(sf)
 Path <- "/home/kaliczp/mnt/disk/Munka/2022DINPIBörzsöny/Adatok/LiDAR_adat/Nyers_pontfelho_UTM34"
 
-for(FileName in dir(Path)) {
+allFiles <- dir(Path, pattern = "laz$")
+
+for(FileName in allFiles) {
     f <- paste(Path, FileName, sep = "/")
     las <- readLAS(f)
     bboxpoints <- st_bbox(las)
     bboxarea <- st_sf(data.frame(FileNr = substr(FileName, 11, 12),
                                  geom = st_as_sfc(bboxpoints)))
-    if(FileName == "RAW_LiDAR_01.las") {
+    if(FileName == allFiles[1]) {
         bboxareaall <- bboxarea
     } else {
         bboxareaall <- rbind(bboxareaall, bboxarea)
