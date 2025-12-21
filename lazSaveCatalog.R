0d12444203ff3fdea70491a8e9453cbd333d2895
Péter Kalicz <kaliczp@gmail.com>
2025-12-21 18:18:19 +0100

    Reshape tiling script
    No progress, chunk size defined, x,y bottm

--- lazSaveCatalog.R
+++ lazSaveCatalog.R
@@ -4,14 +4,15 @@ library(lidR)
 library(future)
 plan(multicore, workers = nothreads)
 set_lidr_threads(nothreads)
+opt_progress(ctg) <- FALSE
 cat("Configured lidR threads: ")
 get_lidr_threads()
-ctg <- readLAScatalog("vgyok/")
+ctg <- readALScatalog("ori")
 st_crs(ctg) <- 23700
 opt_chunk_buffer(ctg) <- 0
-opt_chunk_size(ctg) <- 0
+opt_chunk_size(ctg) <- 100
 opt_laz_compression(ctg) <- TRUE
 opt_filter(ctg) <- "-drop_z_below 1"
-opt_output_files(ctg) <- "vgylaz/{ORIGINALFILENAME}"
+opt_output_files(ctg) <- "newlaz/{XLEFT}_{YBOTTOM}"
 newctg = catalog_retile(ctg)
 cat("End script.\n")
